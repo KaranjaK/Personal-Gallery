b679f1275ed1818a67b9248992782d601cb26744
KK <karanjakelvin77@gmail.com>
2022-03-28 16:00:06 +0300

    It will test the methods in the Category module

--- personal/tests.py
+++ personal/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Category, Location, Image
 
 # Create your tests here.
+# Testing the Category Class and Methods
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category(name='mtaa')
+        self.category.save_category
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+    
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
