db847e1e8fab50d318bb3b77a5ab28d869f20f46
KK <karanjakelvin77@gmail.com>
2022-03-28 17:23:35 +0300

    It will add tests

--- personal/tests.py
+++ personal/tests.py
@@ -5,7 +5,7 @@ from .models import Category, Location, Image
 # Testing the Category Class and Methods
 class CategoryTest(TestCase):
     def setUp(self):
-        self.category = Category(name='mtaa')
+        self.category = Category(id='3',name='mtaa')
         self.category.save_category
 
     def test_instance(self):
@@ -20,3 +20,32 @@ class CategoryTest(TestCase):
         self.category.delete_category()
         category = Category.objects.all()
         self.assertTrue(len(category) == 0)
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location = Location(id='1',name='Nairobi')
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_get_locations(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.get_locations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_update_location(self):
+        new_location = 'Mombasa'
+        self.location.update_location(self.location.id, new_location)
+        changed_location = Location.objects.filter(name='Mombasa')
+        self.assertTrue(len(changed_location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
