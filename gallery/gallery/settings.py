8808cccbd40fb5997c1e0246d698e5deb641989d
KK <karanjakelvin77@gmail.com>
2022-03-28 12:25:23 +0300

    It will add content to the navbar and base html files

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'personal.apps.PersonalConfig',
+    'personal',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
